f2f49162ca6e0dc51f976973e21ca6b639ea0fd3
Md-waliullah499 <152758243+Md-waliullah499@users.noreply.github.com>
2025-07-08 15:15:23 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
   ownerUsernames: ["MOHAMMADNAYAN"],//Your Username
   ownerNumber: "+8801615298449",//Your Number
-  admin: ["5920596290", "8179590682"], // your id
+  admin: ["6509423891", "8179590682"], // your id
   prefix: "/", // dont change prefix 
-  telegramBotToken: "token",//Your Bot Tokens
+  telegramBotToken: "8160978445:AAFV1lgcKrI7EpMQh_w1uQ60nY_Bhg0rWdE",//Your Bot Tokens
   imageUrl: "https://i.postimg.cc/WpmJhNVg/received-383729254132460.jpg",//Thumbnail Url
   po0rt: process.env.PORT || 8053 || 8053, // port
   mess: {
